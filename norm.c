aa33a42f3bfc4826501ace0953a6b8ceba3dd32e
Martin Evrard <mrtnevrard@gmail.com>
2015-11-10 14:53:45 +0100

    ajout des 3 fichiers principaux du programme

---
+++ norm.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <string.h>
+#include "header.h"
+
+
+double* vecprod(double vec1[3],double vec2[3]){
+  
+  double *prod;
+   prod = (double*)calloc( 3,sizeof(double)); 
+  if((vec1[0]-vec2[0])==0 && (vec1[1]-vec2[1])==0 && (vec2[2]-vec2[2])==0){
+
+prod[0]=0;
+  prod[1]=0;
+  prod[2]=0;
+
+  }
+  else{
+  
+ 
+  prod[0]=vec1[1]*vec2[2]-vec1[2]*vec2[1];
+  prod[1]=vec1[2]*vec2[0]-vec2[2]*vec1[0];
+  prod[2]=vec1[0]*vec2[1]-vec2[0]*vec1[1];
+
+  }
+  
+  return prod;
+}
---
+++ view.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <string.h>
+
+
+
+int main(){
+
+
+  FILE* fichier1,*fichier2;
+  int num,type,n_elements,i;
+  double R[3],vec1[3],vec2[3],norm[3],surface;
+
+  fichier2=fopen("view1.pos","w");
+  fichier1=fopen("test_norm.dat","r");
+
+
+  fprintf(fichier2,"View \"Field\" {\n");
+  
+  fscanf(fichier1,"%d\n",&n_elements);
+
+  for(i=0 ; i<n_elements ; i++){
+  fscanf(fichier1,"%d %d %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",&num,&type,&R[0],&R[1],&R[2],&vec1[0],&vec1[1],&vec1[2],&vec2[0],&vec2[1],&vec2[2],&norm[0],&norm[1],&norm[2],&surface);
+
+  fprintf(fichier2,"VP(%lf,%lf,%lf){%lf,%lf,%lf}\;\n",R[0],R[1],R[2],norm[0],norm[1],norm[2]);
+  
+  }
+  fprintf(fichier2,"};");
+  
+  fclose(fichier1);
+  fclose(fichier2);
+  return 0;
+  }
